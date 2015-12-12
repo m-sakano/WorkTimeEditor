77fcd3a6c8fcde36eab1cdd12d20cc02b8a4fb81
m-sakano@bfts.co.jp <m-sakano@bfts.co.jp>
2015-12-13 03:34:03 +0900

    Do not neet description for editing time records

--- addDynamoDBItem.php
+++ addDynamoDBItem.php
@@ -3,15 +3,24 @@
 require_once('config.php');
 
 function addDynamoDBItem($client,$email,$unixTime,$attendance,$description) {
-	try {
-		$result = $client->putItem(array(
-		    'TableName' => DynamoDB_WORKTIME_TABLE,
-		    'Item' => array(
+	if ($description == '') {
+		$item = array(
+		        'Email'      => array('S' => $email),
+		        'UnixTime'    => array('N' => $unixTime),
+		        'Attendance'   => array('S' => $attendance)
+		    );
+	} else {
+				$item = array(
 		        'Email'      => array('S' => $email),
 		        'UnixTime'    => array('N' => $unixTime),
 		        'Attendance'   => array('S' => $attendance),
 		        'Description' => array('S' => $description)
-		    )
+		    );
+	}
+	try {
+		$result = $client->putItem(array(
+		    'TableName' => DynamoDB_WORKTIME_TABLE,
+		    'Item' => $item
 		));
 	} catch (exception $e) {
 		echo 'DynamoDB登録の例外：', $e->getMessage(), "\n";
